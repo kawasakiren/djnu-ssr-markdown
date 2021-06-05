1bf659e671e3927187b7dd009e5f990c053e829e
河崎蓮 <kawasakiren@kawasakirennoMacBook-Pro.local>
2021-06-05 20:32:08 +0900

    DRF-permission setting

--- config/blog/views.py
+++ config/blog/views.py
@@ -1,14 +1,17 @@
 from rest_framework import viewsets
 from blog.models import Tag, Article
+from blog.permissions import IsAdminOrReadOnly
 from blog.serializers import ArticleSerializer, TagSerializer
 
 # Create your views here.
 class TagViewSet(viewsets.ModelViewSet):
 	serializer_class = TagSerializer
 	queryset = Tag.objects.all()
+	permission_classes = (IsAdminOrReadOnly,)
 	
 
 class ArticleViewSet(viewsets.ModelViewSet):
 	queryset = Article.objects.filter(is_public=True)
 	serializer_class = ArticleSerializer
+	permission_classes = (IsAdminOrReadOnly,)
 	lookup_field = 'slug'
