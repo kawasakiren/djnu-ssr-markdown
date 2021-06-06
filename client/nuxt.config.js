59bb2a9f52f903c3d87ddcea58596436592b9e43
河崎蓮 <kawasakiren@kawasakirennoMacBook-Pro.local>
2021-06-06 17:55:40 +0900

    plugins

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -22,6 +22,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    {src: '~/plugins/getArticles.js'},
+    {src: '~/plugins/getTags.js'}
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
